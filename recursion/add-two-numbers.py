3f514f4d975b7d8f3538533b1c630e647b4b398e
qiwenzx <wwenzhaoqi@gmail.com>
2025-10-12 22:30:00 +0100

    LC-add-two-numbers [Runtime: 3 ms; Memory: 17.8 MB]

--- recursion/add-two-numbers.py
+++ recursion/add-two-numbers.py
@@ -24,4 +24,6 @@ class Solution:
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
 
+            # [7, [0, [8, [None]]]] for example, linked list
+
         return dummy.next
