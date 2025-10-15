5a72389901a1e5dcc1add3aa3b23911419a0edcf
qiwenzx <wwenzhaoqi@gmail.com>
2025-10-15 20:05:09 +0100

    LC-palindrome-number [Runtime: 7 ms; Memory: 18 MB]

--- math/palindrome-number.py
+++ math/palindrome-number.py
@@ -1,8 +1,19 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
+        # if x < 0:
+        #     return False
+
+        # s = str(x)
+        
+        # return s == s[::-1]
+
+        # Method 2
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
 
-        s = str(x)
-        
-        return s == s[::-1]
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x = x // 10
+
+        return x == reversed_half or x == reversed_half // 10
